63abbb39eb735a639891c2ce356b94a53345e696
Luciano Lisboa <lucianoicraft@gmail.com>
2021-12-01 09:53:58 -0300

    Eliminando backup da atualizacao do visualstudio.

--- Backup/GoldenControl/frmHelp.vb
+++
@@ -1,5 +0,0 @@
-﻿Public NotInheritable Class frmHelp
-    Private Sub OKButton_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
-        Me.Close()
-    End Sub
-End Class
